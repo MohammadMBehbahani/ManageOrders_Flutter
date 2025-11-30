a6dc23b1c18b9bfaf8942ed31e4f41dcc3a898a5
mohammad behbahani <mohammadmoghadas.z.bbh@gmail.com>
2025-11-30 13:39:06 +0000

    updt main2

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 //import 'dart:io';
 
  import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:manageorders/srcreen/category_screen.dart';
-// import 'package:manageorders/srcreen/drawer_log_screen.dart';
-// import 'package:manageorders/srcreen/extra_screen.dart';
-// import 'package:manageorders/srcreen/home_screen.dart';
-// import 'package:manageorders/srcreen/manage_left_view.dart';
-// import 'package:manageorders/srcreen/management/management_screen.dart';
-// import 'package:manageorders/srcreen/order/order_list_screen.dart';
-// import 'package:manageorders/srcreen/order/order_screen.dart';
-// import 'package:manageorders/srcreen/product/product_screen.dart';
-// import 'package:manageorders/srcreen/shope_details_screen.dart';
-// import 'package:manageorders/srcreen/topping_screen.dart';
-// import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-// import 'package:window_manager/window_manager.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:manageorders/srcreen/category_screen.dart';
+import 'package:manageorders/srcreen/drawer_log_screen.dart';
+import 'package:manageorders/srcreen/extra_screen.dart';
+import 'package:manageorders/srcreen/home_screen.dart';
+import 'package:manageorders/srcreen/manage_left_view.dart';
+import 'package:manageorders/srcreen/management/management_screen.dart';
+import 'package:manageorders/srcreen/order/order_list_screen.dart';
+import 'package:manageorders/srcreen/order/order_screen.dart';
+import 'package:manageorders/srcreen/product/product_screen.dart';
+import 'package:manageorders/srcreen/shope_details_screen.dart';
+import 'package:manageorders/srcreen/topping_screen.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:window_manager/window_manager.dart';
 
 final RouteObserver<ModalRoute<void>> routeObserver =
     RouteObserver<ModalRoute<void>>();
@@ -33,56 +33,36 @@ void main() async {
   //   sqfliteFfiInit();
   //   databaseFactory = databaseFactoryFfi;
   // }
- // runApp(const ProviderScope(child: ManageOrdersApp()));
+  runApp(const ProviderScope(child: ManageOrdersApp()));
 
- runApp(const MyTestApp());
+// runApp(const MyTestApp());
 }
 
-// class ManageOrdersApp extends StatelessWidget {
-//   const ManageOrdersApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       navigatorObservers: [routeObserver],
-//       debugShowCheckedModeBanner: false,
-//       themeMode: ThemeMode.system,
-//       initialRoute: '/home_screen',
-//       routes: {
-//         '/home_screen': (context) => const HomeScreen(),
-//         '/category_screen': (context) => const CategoryScreen(),
-//         '/extra_screen': (context) => const ExtraScreen(),
-//         '/product_screen': (context) => const ProductScreen(),
-//         '/topping_screen': (context) => const ToppingScreen(),
-//         '/order_screen': (context) => const OrderScreen(),
-//         '/order_list_screen': (context) => const SubmittedOrdersScreen(),
-//         '/management_screen': (context) => const ManagementScreen(),
-//         '/shope_detail_screen': (context) => const ShopDetailsScreen(),
-//         '/manage_left_view_screen': (context) => const ManageLeftViewScreen(),
-//          '/drawer_logs': (context) => const DrawerLogsScreen()
-        
-//       },
-//     );
-//   }
-// }
-
-
-class MyTestApp extends StatelessWidget {
-  const MyTestApp({super.key});
+class ManageOrdersApp extends StatelessWidget {
+  const ManageOrdersApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      navigatorObservers: [routeObserver],
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Text(
-            'Windows Test OK',
-            style: TextStyle(fontSize: 32),
-          ),
-        ),
-      ),
+      themeMode: ThemeMode.system,
+      initialRoute: '/home_screen',
+      routes: {
+        '/home_screen': (context) => const HomeScreen(),
+        '/category_screen': (context) => const CategoryScreen(),
+        '/extra_screen': (context) => const ExtraScreen(),
+        '/product_screen': (context) => const ProductScreen(),
+        '/topping_screen': (context) => const ToppingScreen(),
+        '/order_screen': (context) => const OrderScreen(),
+        '/order_list_screen': (context) => const SubmittedOrdersScreen(),
+        '/management_screen': (context) => const ManagementScreen(),
+        '/shope_detail_screen': (context) => const ShopDetailsScreen(),
+        '/manage_left_view_screen': (context) => const ManageLeftViewScreen(),
+         '/drawer_logs': (context) => const DrawerLogsScreen()
+        
+      },
     );
   }
 }
+
